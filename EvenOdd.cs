16c75bc46a17620d48c07846a5722080c30e38ef
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:41:43 +0530

    Day-5 UC-1 Added

---
+++ EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingProblems
+{
+    public class EvenOdd
+    {
+        public static void EvenOddNumber()
+        {
+            Console.Write("Please enter a number:  ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine(number + " is an odd number");
+            }
+        }
+    }
+}
