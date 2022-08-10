821ebb06c9dbb8ddb0fc4c69ec42bb7007a56bc5
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:51:05 +0530

    Program File  Added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicProgrammingProblems
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LargestThreeNo.LargestThreeNumber();
+            LeapYear.display();
+            PowerOfTwoNumber.PowerOfTwo();
+            SwapTwoNumbers.SwapTwoNum();
+            PrimeFactorsOfNumbers.PrimeFactors();
+            
+            HarmonicNumber.HarmonicNum();
+
+            EvenOdd.EvenOddNumber();
+            VowelOrConstants.CheckVowels();
+
+        }
+    }
+}
