165a94438a0a78495858660ef86bb19645921d17
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:47:04 +0530

    Day-5 UC-4 Added

---
+++ LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingProblems
+{
+        public static class LeapYear
+        {
+              public static void display()
+              {
+                 int CheckYear;
+
+                 Console.WriteLine("Enter Year : ");
+                 CheckYear = Convert.ToInt32(Console.ReadLine());
+
+                 if ((CheckYear % 400) == 0)
+                    Console.WriteLine("{0} is a leap year.\n", CheckYear);
+
+                 else if ((CheckYear % 100) == 0)
+                    Console.WriteLine("{0} is not a leap year.\n", CheckYear);
+
+                 else if ((CheckYear % 4) == 0)
+                    Console.WriteLine("{0} is a leap year.\n", CheckYear);
+
+                 else
+                    Console.WriteLine("{0} is not a leap year.\n", CheckYear);
+              }
+        }
+    
+}
+
