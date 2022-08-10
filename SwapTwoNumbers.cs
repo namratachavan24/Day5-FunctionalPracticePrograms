463ecc6a51a088138dfaef70b7d3d5d913cb5385
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:52:07 +0530

    Day-5 UC-7 Added

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingProblems
+{
+    public class SwapTwoNumbers
+    {
+        public static void SwapTwoNum()
+        {
+            int x = 5, y = 10;
+
+            Console.WriteLine("Before swap x= " + x + " y= " + y);
+
+            x = x * y;
+            y = x / y;
+            x = x / y;
+
+            Console.Write("After swap x= " + x + " y= " + y);
+        }
+    }
+}
