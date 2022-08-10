8b3feb38509699f815030fd78c4f43af4f3bd8a1
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:45:38 +0530

    Add files via upload

---
+++ LargestThreeNo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingProblems
+{
+    public static class LargestThreeNo
+    {
+        public static void LargestThreeNumber()
+        {
+            int num1, num2, num3, largest;
+            Console.WriteLine("Enter the number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number 3");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+                largest = num1;
+            else if
+                (num2 > num1 && num2 > num3)
+                largest = num2;
+            else
+                largest = num3;
+
+            Console.WriteLine("Largest Number among {0},{1} and {2} is {3},num1,num2,num3,largest");
+            Console.ReadKey();
+        }
+    }
+}
+    
